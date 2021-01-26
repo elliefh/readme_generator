64ab85e81ceaf1f19afa5bee591505b919bee290
elliefh <elliefu18@gmail.com>
2021-01-26 14:12:25 -0800

    set up inquirer

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const fs = require("fs");
 const questions = [
     {
         type: 'input',
-        message: 'What is your name?',
-        name: 'personName',
+        message: 'What is your GitHub username?',
+        name: 'userName',
     },
     {
         type: 'input',
-        message: 'When is your birthday?',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'projectName',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project:',
         name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'Which type of license would you like your project to have?',
+        name: 'license',
+        choices: ['MIT License', 'GNU General Public License 3.0', 'Apache License 2.0']
     }
 ];
 
@@ -24,9 +40,14 @@ function init() {
     inquirer
     .prompt(questions)
     .then(response => {
-        console.log(response.personName);
+        console.log(response.userName);
+        console.log(response.email);
+        console.log(response.projectName);
         console.log(response.description);
-        const content = generateMarkdown(response);
+        console.log(response.license);
+
+
+        // const content = generateMarkdown(response);
     }); 
 }
 
